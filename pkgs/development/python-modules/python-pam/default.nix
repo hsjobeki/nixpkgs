a16ab03fc46767a65f59a4610513a3843f5d0590
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-07-20 23:33:53 +0200

    fixup! python3Packages.python-pam: 1.8.4 -> 2.0.2

--- pkgs/development/python-modules/python-pam/default.nix
+++ pkgs/development/python-modules/python-pam/default.nix
@@ -2,11 +2,14 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pam
+, six
+, toml
 }:
 
 buildPythonPackage rec {
   pname = "python-pam";
   version = "2.0.2";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "FirefighterBlu3";
@@ -15,13 +18,22 @@ buildPythonPackage rec {
     sha256 = "sha256-MR9LYXtkbltAmn7yoyyKZn4yMHyh3rj/i/pA8nJy2xU=";
   };
 
+  postPatch = ''
+    substituteInPlace src/pam/__internals.py \
+      --replace 'find_library("pam")' '"${pam}/lib/libpam.so"' \
+      --replace 'find_library("pam_misc")' '"${pam}/lib/libpam_misc.so"'
+  '';
+
   buildInputs = [
     pam
   ];
 
-  postPatch = ''
-    sed "s|find_library(\"pam\")|\"${pam}/lib/libpam.so\"|g" -i pam.py
-  '';
+  propagatedBuildInputs = [
+    six
+    toml
+  ];
+
+  pythonImportsCheck = [ "pam" ];
 
   meta = with lib; {
     description = "Python pam module supporting py3 (and py2)";
